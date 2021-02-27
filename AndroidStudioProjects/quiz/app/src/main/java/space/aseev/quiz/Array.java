ccde2886aecde1fc4826dfecfe067555c1b8c9cc
ArtyseevWork <ArtyseevWork@gmail.com>
2021-02-27 17:38:44 +0200

    1739270221 lesson26

---
+++ AndroidStudioProjects/quiz/app/src/main/java/space/aseev/quiz/Array.java
@@ -0,0 +1,32 @@
+package space.aseev.quiz;
+
+public class Array {
+    final int [] images1 = {
+            R.drawable.onelevel_0,
+            R.drawable.onelevel_1,
+            R.drawable.onelevel_2,
+            R.drawable.onelevel_3,
+            R.drawable.onelevel_4,
+            R.drawable.onelevel_5,
+            R.drawable.onelevel_6,
+            R.drawable.onelevel_7,
+            R.drawable.onelevel_8,
+            R.drawable.onelevel_9
+    };
+
+    final int[] power1 = {0,1,2,3,4,5,6,7,8,9};
+    final int[] text1 = {
+           R.string.zero,
+           R.string.one,
+           R.string.two,
+           R.string.three,
+           R.string.four,
+           R.string.five,
+           R.string.six,
+           R.string.seven,
+           R.string.eight,
+           R.string.nine,
+
+    };
+
+}
